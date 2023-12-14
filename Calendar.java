12c6d5f79fdc99f4bf4bb5aabf35c06f64046a07
Khean Maree Casumpang Vallejera <97454452+Khean28@users.noreply.github.com>
2023-12-14 09:39:21 -0800

    Add files via upload

---
+++ Calendar.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Map;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
+public class Calendar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TimeblockManager timeblockManager = new TimeblockManager();
+    TreeMap<Integer, Entry> allEntries = new TreeMap<>();
+        Queue<UnscheduledEntry> unscheduledEntriesQueue = new LinkedList<>();
+
+        // scheduled entries
+        System.out.println("What are your scheduled agendas today?");
+        while (true) {
+            System.out.print("\nEnter Scheduled Task Name (exit to finish): ");
+            String taskName = scanner.nextLine();
+
+            if (taskName.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            System.out.print("Enter start time (HH:mm): ");
+            String startTimeStr = scanner.nextLine();
+            System.out.print("Enter end time (HH:mm): ");
+            String endTimeStr = scanner.nextLine();
+
+            try {
+                LocalTime startTime = LocalTime.parse(startTimeStr);
+                LocalTime endTime = LocalTime.parse(endTimeStr);
+
+                timeblockManager.addTimeBlock(startTime, endTime, taskName);
+                ArrayList<Integer> timeslotsToUpdate = new ArrayList<>();
+                int startMinutes = startTime.getHour() * 4 + startTime.getMinute();
+                int endMinutes = endTime.getHour() * 4 + endTime.getMinute();
+
+                for (int i = startMinutes; i <= endMinutes; i++) {
+                    timeslotsToUpdate.add(i);
+                }
+
+                timeblockManager.updateTimeslots(timeslotsToUpdate);
+
+
+                allEntries.put(startMinutes, new ScheduledEntry(taskName, startTime, endTime));
+                allEntries.put(endMinutes, null);
+            } catch (DateTimeParseException e) {
+                System.out.println(e);
+            }
+        }
+
+        // unscheduled entries
+        System.out.println("\nWhat are your unscheduled agendas today?");
+
+        while (true) {
+            System.out.print("\nEnter Unscheduled Task Name (exit to finish): ");
+            String taskName = scanner.nextLine();
+
+            if (taskName.equalsIgnoreCase("exit")) {
+                break;
+            }
+            
+            try {
+                System.out.print("Enter due time (HH:mm): ");
+                String dueTime = scanner.nextLine();
+
+                int units = 0;
+                int unitsPerTimeslot = 0;
+
+                try {
+                    System.out.print("Enter units: ");
+                    units = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Enter units per timeslot: ");
+                    unitsPerTimeslot = scanner.nextInt();
+                    scanner.nextLine();
+                } catch (InputMismatchException e) {
+                    System.out.println(e);
+                }
+                LocalTime duetime = LocalTime.parse(dueTime);
+                unscheduledEntriesQueue.add(new UnscheduledEntry(taskName, dueTime, units, unitsPerTimeslot));
+            } catch (DateTimeParseException e) {
+                System.out.println(e);
+            }
+        }
+
+        UnscheduledEntryStrategy unscheduledEntryStrategy = new UnscheduledEntryStrategy();
+        unscheduledEntryStrategy.scheduleUnscheduledEntries(unscheduledEntriesQueue, timeblockManager, allEntries);
+
+
+        displayAllEntries(allEntries);
+
+        // timeblockManager.displayOccupiedTimeslots();
+    }
+
+    private static void displayAllEntries(TreeMap<Integer, Entry> allEntries) {
+        System.out.println("\nAll Entries:");
+    
+        for (int i = 0; i < TimeblockManager.MAX_TIME_SLOTS; i++) {
+            int hour = i / 4;
+            int minute = (i % 4) * 15;
+    
+            String timeBlock = String.format("%02d:%02d", hour, minute);
+    
+            Map.Entry<Integer, Entry> floorEntry = allEntries.floorEntry(i);
+            String taskName = (floorEntry != null && floorEntry.getValue() != null) ? floorEntry.getValue().getName() : "N/A";
+            String status = (floorEntry != null && floorEntry.getValue() != null) ? "Occupied" : "Available";
+    
+            System.out.println("Time Slot " + i + " (" + timeBlock + "): " +
+                    status + " - Task: " + taskName);
+        }
+    }
+}
+
